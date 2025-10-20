836e530fa3ef7205207c85583327f4930d82bb6b
younghun <disrespect901@gmail.com>
2025-10-20 17:39:31 +0900

    Update app_ML.py

--- app_ML.py
+++ app_ML.py
@@ -1,25 +1,25 @@
 import streamlit as st
+import joblib 
 import pandas as pd
-import joblib
 
-def run_ML():
+def run_ML() :
     st.subheader('수익 예측')
     st.info('아래 데이터를 넣으시면, 수익을 예측합니다.')
 
     rnd = st.number_input('연구개발비', min_value=0)
     admin = st.number_input('운영비', min_value=10000)
-    marketing = st.number_input('마케팅비',min_value=0)
-    state = st.radio('주 선택', ['California', 'Florida', 'New York'])
+    marketing = st.number_input('마케팅비', min_value=0)
+    state = st.radio('주 선택', ['California','Florida','New York' ] ) 
 
-    if st.button('수익예측'):
+    if st.button('수익예측') :
         pipe = joblib.load('./model/pipe.pkl')
 
-        new_data = [{'R&D Spend':rnd, 'Administration' : admin, 'Marketing Spend':marketing}]
-        
-        df_new = pd.DataFrame(data = new_data)
+        new_data = [{'R&D Spend':rnd , 'Administration':admin, 'Marketing Spend':marketing, 'State':state}]
+
+        df_new = pd.DataFrame(data= new_data)
 
         y_pred = pipe.predict(df_new)
 
         y_pred = y_pred[0]
 
-        st.info(format(round(y_pred),',')+ '달러 입니다')
+        st.info( '예상 수익은 ' + format( round(y_pred) , ',' ) + '달러 입니다')
